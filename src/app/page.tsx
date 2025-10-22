360aa1c9942797e4f2a694b4ba4a88eecdad39fb
LizaKlev <elizabeth.klevakina@gmail.com>
2025-10-22 23:32:45 +0300

    buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import { Input } from '../components/ui/input'
+import { Button } from '../components/ui/button'
 
 export default function Home() {
   return (
     <div>
       <p>mememe</p>
       <Input placeholder='Search...' />
+      <Button variant='outline'>Button</Button>
+      <Button variant='outline'>Outline</Button>
+      <Button variant='secondary'>Secondary</Button>
     </div>
   )
 }
