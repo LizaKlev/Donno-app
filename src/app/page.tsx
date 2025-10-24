f107f969e5e36e6b62f8a24e644387f7f132811a
LizaKlev <elizabeth.klevakina@gmail.com>
2025-10-24 17:19:08 +0300

    layout and some componenets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,16 @@
 import { Input } from '../components/ui/input'
 import { Button } from '../components/ui/button'
+import { InputGroupButton } from '../components/ui/input-group'
 
 export default function Home() {
   return (
     <div>
       <p>mememe</p>
       <Input placeholder='Search...' />
-      <Button variant='outline'>Button</Button>
-      <Button variant='outline'>Outline</Button>
-      <Button variant='secondary'>Secondary</Button>
+      <Button variant='primary'>Primary Button</Button>
+      <Button variant='secondary'>Secondary Button</Button>
+      <Input placeholder='Search...' />
+      <InputGroupButton variant='primary'>Go</InputGroupButton>
     </div>
   )
 }
