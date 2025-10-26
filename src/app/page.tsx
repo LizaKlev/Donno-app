f0daa3fbeace74c0cac4cabbc2ac3d1405166017
LizaKlev <elizabeth.klevakina@gmail.com>
2025-10-26 23:33:12 +0200

    breadcrumbs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
 import { Input } from '../components/ui/input'
 import { Button } from '../components/ui/button'
 import { InputGroupButton } from '../components/ui/input-group'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '../components/ui/breadcrumb'
 
 export default function Home() {
   return (
     <div>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink href='/'>Home</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink href='/components'>Components</BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Breadcrumb</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
       <p>mememe</p>
       <Input placeholder='Search...' />
       <Button variant='primary'>Primary Button</Button>
